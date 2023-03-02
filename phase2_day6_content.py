b6d2adea990d6aef126aec53168caa0d3079d36c
LakkojuSantosh <124041597+LakkojuSantosh@users.noreply.github.com>
2023-03-02 15:43:35 +0530

    Add files via upload

---
+++ phase2_day6_content.py
@@ -0,0 +1,39 @@
+Graphs:
+   ---> It is a Non-linear data structure
+   
+Adjacency : Connection should be there must be a route or path
+ 
+Types of Graphs:-
+        1.Undirected graphs
+        2.Directed graphs
+        3.Weigh graphs
+
+
+         (0) -------(1)
+          | \        |\
+          |  \       | \
+          |    \     | (4)
+          |      \   | /
+          |         \|/
+         (2)------- (3)
+
+
+
+
+
+Representation of Graphs:
+    1.List of edges
+    2.Adjacency List
+    3.Adjacency Matrix
+
+***BFS & DFS :
+In Breadth First Search, visit and then move on to all the vertices 
+whereas ,in DFS after visitindg a node select on adjacent vertex and exlore it completely move on with other vertex
+
+
+*****Dijkstra algorithm:
+   1. It comes under greedy method where solution for the given problem will be figured out stage by stage.
+   2. Dijkstra algorithm helps to find out the shortest path or distance from one vertex to all the other vertices.
+
+ -----> Drawback:
+         If weight of any edge is a negative number,dijkstra won't work for that.
